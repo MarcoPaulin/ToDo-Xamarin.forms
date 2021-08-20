8b331b3321d770d11ab134541243d84c32e36e20
Marco Paulin <m.paulin@biodiv-wind.com>
2021-08-20 14:56:25 +0200

    Ajoutez des fichiers projet.

---
+++ ToDo/ToDo/View/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.View;
+using Xamarin.Forms;
+
+namespace ToDo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		int count = 0;
+		void Button_Clicked(object sender, System.EventArgs e)
+		{
+			count++;
+			((Button)sender).Text = $"You clicked {count} times.";
+		}
+
+		async void Create_Todo(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new TodoView());
+		}
+	}
+}
---
+++ ToDo/ToDo/View/TodoView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TodoView : ContentPage
+	{
+		public TodoView()
+		{
+			InitializeComponent();
+		}
+	}
+}
